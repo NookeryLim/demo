677fa8a80a4c306f0dd335fbb7b621a786a11586
NookeryLim <dlasnrk@naver.com>
2022-11-11 17:58:50 +0900

    sns로그인, 글쓰기 권한 화면 구성

--- src/main/java/com/zozoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/zozoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.zozoldu.book.springboot.web;
 
+import com.zozoldu.book.springboot.config.auth.dto.SessionUser;
 import com.zozoldu.book.springboot.service.posts.PostsService;
 import com.zozoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장. 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성. 즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+        if (user != null) { //세션에 저장된 값이 있을 때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,4 +40,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
