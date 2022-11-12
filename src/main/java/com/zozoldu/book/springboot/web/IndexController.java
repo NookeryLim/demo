0736bf817ab45693fbb334e14b006cbad0fd9cbc
NookeryLim <dlasnrk@naver.com>
2022-11-12 15:13:44 +0900

    어노테이션 기반 개선 2

--- src/main/java/com/zozoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/zozoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.zozoldu.book.springboot.web;
 
+import com.zozoldu.book.springboot.config.auth.LoginUser;
 import com.zozoldu.book.springboot.config.auth.dto.SessionUser;
 import com.zozoldu.book.springboot.service.posts.PostsService;
 import com.zozoldu.book.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장. 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+//    public String index(Model model) { //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장. 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+    public String index(Model model, @LoginUser SessionUser user) { //기존에 (User)httpSession.getAttribute("user")로 가져오던 세셜 정보 값이 개선됨. 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됨
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성. 즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성. 즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+
         if (user != null) { //세션에 저장된 값이 있을 때만 model에 userName으로 등록
             model.addAttribute("userName", user.getName());
         }
